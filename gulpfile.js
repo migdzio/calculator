944f89bb05077f9414bdb26f77354606efa9ba67
M Bartnik <michal.bartnik@gmail.com>
2016-08-26 10:14:35 +0200

    Wrzucam podstawowe pliki

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+var gulp = require('gulp');
+var sass = require("gulp-sass");
+var rename = require("gulp-rename");
+var sourcemaps = require('gulp-sourcemaps');
+
+Files = {
+    scss:
+        ['./sass/_init.scss', './sass/_header.scss', './sass/_content.scss', './sass/_footer.scss'],
+    js:
+        ['./js/app.js'],
+    html: ['./index.html']
+};
+
+
+
+gulp.task('sass', function() {
+    
+    return gulp.src('./sass/import.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(sourcemaps.write())
+        .pipe(rename('style.css'))
+        .pipe(gulp.dest('./css'));
+    
+});
+
+gulp.task('default', function() {
+
+    gulp.watch(Files.html);
+    gulp.watch(Files.scss, ["sass"]);
+    
+});
